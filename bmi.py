a6df04825f29a871e6ce9ce054d68a91b3ca2196
Yuu18id <211112007@students.mikroskil.ac.id>
2022-05-07 15:21:19 +0700

    Add some print

--- bmi.py
+++ bmi.py
@@ -17,14 +17,21 @@ BMI = berat_badan / tinggi_badan **2
 
 if (BMI <= 18.5):
     keterangan = ("Kurus")
+    print ("##########################################")
+    print ("Perbanyak Makanan yang Bergizi ya!")
 elif (BMI >=18.5 and BMI <=22.9):
     keterangan = ("Ideal")
+    print ("##########################################")
+    print ("Pertahankan Pola Hidup Sehatnya :)")
 elif (BMI >=23 and BMI <=29.9):
     keterangan = ("Gemuk")
+    print ("##########################################")
+    print ("Perbanyak Olahraga dan Makanan Bergizi Ya!")
 elif (BMI >=30):
     keterangan = ("Obesitas")
+    print ("##########################################")
+    print ("Perbanyak Olahraga dan Makanan Bergizi Ya!")
 
 #output
-print ("##########################################")
 print ("BMI anda bernilai", BMI, "termasuk kedalam kategori", keterangan)
 print ("##########################################")
